964918f50f3745daadc4b768b26db55d210e611a
Ravi Kumar <ravi14577@gmail.com>
2023-03-22 19:37:06 +0530

    add button

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Todolist = (props) =>{
-    return <li>{props.text}</li>;
+    return (
+    <>
+    <div className="todo_style">
+        <i class="fa fa-times" aria-hidden="true"/>
+    <li>{props.text}</li>;
+    </div>
+    
+    </>
+    );
 };
 
 export default Todolist;
